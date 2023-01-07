aade826824a2b318c046a489aea7b67c2725f74a
Pham Quan Tu <quantu211154@gmail.com>
2023-01-07 12:10:27 +0700

    Fix cookie

--- dist/utils/send-jwt-refresh-token.js
+++ dist/utils/send-jwt-refresh-token.js
@@ -3,13 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendJWTRefreshToken = void 0;
 const create_jwt_token_1 = require("./create-jwt-token");
 const sendJWTRefreshToken = (res, user) => {
-    res.cookie(process.env.TCLOUD_REFRESH_TOKEN_NAME, (0, create_jwt_token_1.createJWTToken)('refreshToken', user)
-    // {
-    //   httpOnly: true,
-    //   secure: true,
-    //   sameSite: 'lax',
-    // }
-    );
+    res.cookie(process.env.TCLOUD_REFRESH_TOKEN_NAME, (0, create_jwt_token_1.createJWTToken)('refreshToken', user), {
+        path: '/',
+        httpOnly: true,
+        secure: true,
+        sameSite: true,
+        signed: true,
+        maxAge: 24 * 60 * 60 * 1000,
+    });
     console.log('Cookie set');
 };
 exports.sendJWTRefreshToken = sendJWTRefreshToken;
--- src/utils/send-jwt-refresh-token.ts
+++ src/utils/send-jwt-refresh-token.ts
@@ -5,12 +5,15 @@ import { createJWTToken } from './create-jwt-token'
 export const sendJWTRefreshToken = (res: Response, user: User) => {
   res.cookie(
     process.env.TCLOUD_REFRESH_TOKEN_NAME as string,
-    createJWTToken('refreshToken', user)
-    // {
-    //   httpOnly: true,
-    //   secure: true,
-    //   sameSite: 'lax',
-    // }
+    createJWTToken('refreshToken', user),
+    {
+      path: '/',
+      httpOnly: true,
+      secure: true,
+      sameSite: true,
+      signed: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    }
   )
   console.log('Cookie set')
 }
